56d220cbe893342f2ae4f815daccae2f7f044610
cycy <2276928296@qq.com>
2017-06-09 09:36:15 +0800

    first commit

---
+++ web/src/modules/login/LoginComponent.js
@@ -0,0 +1,66 @@
+// var React = require('react')
+// var Component = React.Component;
+// var React, {Component} = require('react');
+
+// var ReactRouter = require('react-route');
+
+// var {Router, Route, Link} = ReactRouter
+
+// var Router = ReactRouter.Router
+// var Route = ReactRouter.Route
+// var Link = ReactRouter.Link
+
+// import {Router, Route, Link} from 'React-Router'
+
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import * as loginActions from './LoginAction'
+import SpinnerComponent from '../spinner/SpinnerComponent'
+
+// @connect(
+//     state => ({
+//         loading: state.login.loading
+//     }),
+//     loginActions
+// )
+
+class LoginComponent extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    loginHandler(){
+        // console.log(loginActions)
+        // this.router.push('register')
+        // if(!this.refs.username){
+        //     //show up dialog => username cannot empty
+        //     return
+        // } else if(!this.refs.password){
+        //     //show up dialog => password cannot empty
+        //     return 
+        // }
+        
+        this.props.login(this.refs.username.value, this.refs.password.value)
+       console.log(this.props)
+    }
+
+    render(){
+        return(
+            <div className="login">
+                <ul>
+                    <li><input type="text" ref="username"/></li>
+                    <li><input type="text" ref="password"/></li>
+                    <li><input type="button" value="登录" onClick={this.loginHandler.bind(this)}/></li>
+                    <li>{this.props.loading + ''}</li>
+                </ul>
+                <SpinnerComponent show={this.props.loading}/>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    loading: state.login.loading
+})
+export default connect(mapStateToProps, loginActions)(LoginComponent)
+// export default LoginComponent
