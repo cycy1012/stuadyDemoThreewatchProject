5a93c159e9ed82691d6240ba81fefbd9a3a65e85
cycy <2276928296@qq.com>
2017-06-12 10:36:59 +0800

    6/12 覆盖一次

--- web/src/modules/app/AppComponent.js
+++ web/src/modules/app/AppComponent.js
@@ -4,6 +4,7 @@ import React, {Component} from 'react'
 var ReactRouter = require('react-router');
 var {Router,Route,hashHistory,Link,IndexRoute,browserHistory} = ReactRouter;
 import "./App.scss"
+import FooterComponent from '../footer/FooterComponent.js'
 import pic1 from '../../static/imgs/1.png'
 import pic2 from '../../static/imgs/2.png'
 
@@ -22,10 +23,10 @@ class AppComponent extends Component{
             	<main className="app_main">
             		<section className="banner">
 						<div className="banner_mask">
-							<img src={pic1} alt="" />
-							<img src={pic2} alt="" />
-							<img src={pic1} alt="" />
-							<img src={pic2} alt="" />
+							<img src={require('../../static/imgs/5.png')} alt="" />
+							<img src={require('../../static/imgs/6.png')} alt="" />
+							<img src={require('../../static/imgs/5.png')} alt="" />
+							<img src={require('../../static/imgs/6.png')} alt="" />
 						</div>
 	            	</section>
 	            	<div className="linkPic"><Link to=""><img src={require('../../static/imgs/3.png')} alt="" /></Link></div>
@@ -43,15 +44,15 @@ class AppComponent extends Component{
 	            		<div className="recommend">
 							<ul>
 								<li>
-									<div><img src={pic1} alt="" /></div>
+									<div><img src={require('../../static/imgs/4.png')} alt="" /></div>
 									<p>浪琴浪琴运动系列33913987 男表</p>
 								</li>
 								<li>
-									<div><img src={pic2} alt="" /></div>
+									<div><img src={require('../../static/imgs/nanshi.jpg')} alt="" /></div>
 									<p>浪琴浪琴运动系列33913987 男表</p>
 								</li>
 								<li>
-									<div><img src={pic1} alt="" /></div>
+									<div><img src={require('../../static/imgs/nvshi.jpg')} alt="" /></div>
 									<p>浪琴浪琴运动系列33913987 男表</p>
 								</li>
 								<li>
@@ -71,45 +72,46 @@ class AppComponent extends Component{
 	            	</div>
 	        		<div className="watchOption">
 						<div>
-							<Link to="">
+							<Link to="" className="watch_Option watch_Option1">
 								<span>男表</span>
-								<span>Men</span>
+								<span>MEN</span>
 							</Link>
 						</div>
 						<div>
-							<Link to="">
-								<span>男表</span>
-								<span>Men</span>
+							<Link to="" className="watch_Option watch_Option2">
+								<span>女表</span>
+								<span>WOMEN</span>
 							</Link>
 						</div>
 						<div>
-							<Link to="">
-								<span>男表</span>
-								<span>Men</span>
+							<Link to="" className="watch_Option watch_Option3">
+								<span>机械表</span>
+								<span>MECHANICS</span>
 							</Link>
 						</div>
 						<div>
-							<Link to="">
-								<span>男表</span>
-								<span>Men</span>
+							<Link to="" className="watch_Option watch_Option4">
+								<span>石英表</span>
+								<span>QUARTZ</span>
 							</Link>
 						</div>
+						<span className="midMask"></span>
 	        		</div>
 	            	<div className="series-watch">
-	            		<div>
-	            			<Link to=""><img src="" alt="" /></Link>
+	            		<div className="series_bigPicture">
+	            			<Link to=""><img src={require('../../static/imgs/9.png')} alt="" /></Link>
 	            		</div>
 	            		<ul>
-	            			<li><Link to=""><img src="" /></Link></li>
-	            			<li><Link to=""><img src="" /></Link></li>
-	            			<li><Link to=""><img src="" /></Link></li>
+	            			<li><Link to=""><img src={require('../../static/imgs/4.png')} /></Link></li>
+	            			<li><Link to=""><img src={require('../../static/imgs/4.png')} /></Link></li>
+	            			<li><Link to=""><img src={require('../../static/imgs/4.png')} /></Link></li>
 	            		</ul>
 	            		<div className="series_title">
-	            			<h4>星座系列</h4>
+	            			<h4 className="series_title_h4">星座系列</h4>
 	            		</div>
 	            	</div>
-	            	<article>
-	            		<div class="article_title">
+	            	<article className="app_article">
+	            		<div className="app_article_title">
 	            			<ul>
 	            				<li><Link to="">人气</Link></li>
 	            				<li><Link to="">全新</Link></li>
@@ -117,12 +119,19 @@ class AppComponent extends Component{
 	            			</ul>	
 	            		</div>
 	            		<div className="getlist">
-	            			<ul className="list"></ul>
+	            			<ul className="list">
+	            				<li>
+	            					<Link to="" className="list_watch_picture"><img src="" /></Link>
+	            					<Link to=""><p className="list_watch_name">星座系列1460.75.00欧米茄  石英女表 星座系列1460.75.00</p></Link>
+	            					<p>￥15000.00</p>
+	            				</li>
+	            			</ul>
 	            			<ul className="list hide"></ul>	
 	            			<ul className="list hide"></ul>
 	            		</div>
 	            	</article>	
             	</main>
+            	<FooterComponent/>
             </div>
         )
     }
